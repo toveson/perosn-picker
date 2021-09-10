84db3196ce1c3f16431bff2ed6536cff03d5f32c
Trever O <toveson9522@gmail.com>
2021-09-09 23:35:05 -0600

    entered names are now created when enter names is clicked

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 let names = [];
 
 $('#nameBtn').click(function () {
+    // getting the entered name
     let enteredName = $('#nameInput').val();
+    // saving entered name to names array
     names.push(enteredName);
+    // clearing input field
+    $('#nameInput').val('');
+    // Creates dropdown for the last name entered
+    $('#names').append(
+        '<button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="' + names[names.length - 1] + '">' +
+        names[names.length - 1] +
+        '</button>' +
+        '<div class="dropdown-menu" aria-labelledby="dropdownMenuButton"' + '>' +
+        '<a class="dropdown-item" href="#"> Times picked: 0 </a + >' +
+        // <a class="dropdown-item" href="#">Another action</a>
+        '</div>' +
+        '</div >'
+    );
+    
     console.log(names);
 });
