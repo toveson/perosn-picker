b5baf58f42c59732cec2c3460e8b62c1221bc95a
Trever O <toveson9522@gmail.com>
2021-09-12 17:52:21 -0600

    added shuffle to picking person array

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ $('#nameBtn').click(function () {
     names.push({ name: enteredName, timesPicked: 0 });
     // clearing input field
     $('#nameInput').val('');
-    // Creates dropdown for the last name entered
+    // Creates dropdown button for the last name entered
     $('#names').append(
         '<button class="btn btn-secondary dropdown-toggle nameChoices" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="' + names[names.length - 1].name + '">' +
         names[names.length - 1].name +
@@ -34,6 +34,7 @@ $('#pickName').click(function () {
     names.forEach(names => {
         let timesToAdd = count - names.timesPicked;
         if (count >= timesToAdd) {
+            // adds name to array x times, depending on how many times it's been picked before
             for (let i = 0; i < timesToAdd; i++) {
                 pickingFrom.push(names.name);
             }
@@ -41,6 +42,17 @@ $('#pickName').click(function () {
     });
     console.log(pickingFrom);
 
+    // shuffles pickingFrom before choosing a person using Fisher-Yates Shuffle Algorithm
+    // great visualization of how this algorithm works https://bost.ocks.org/mike/shuffle/
+    let i = pickingFrom.length, j, temp;
+    while(--i > 0){
+        j = Math.floor(Math.random() * (i + 1));
+        temp = pickingFrom[j];
+        pickingFrom[j] = pickingFrom[i];
+        pickingFrom[i] = temp;
+    }
+    console.log('shuffled: ', pickingFrom);
+    
     // Picks a random name
     let randomNumber = names[Math.floor(Math.random() * names.length)]
     // goes through each named button
