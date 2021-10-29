81877277f6a026c1485095602fcaa8dbed87eada
Trever O <toveson9522@gmail.com>
2021-10-28 21:23:30 -0600

    connected to mongodb

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+require('dotenv').config({ path: '../.env' });
+// console.log(`>>>>>----- Mongo URI: ${process.env.MONGODB_URI} -----<<<<<`)
 
 app.use(express.static('../client'));
 
+// db 
+const mongoose = require('mongoose');
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  }
+);
+
+mongoose.connection.on('connected', () => {
+console.log('>>>>>----- Mongoose is connected -----<<<<<')
+})
+
+// routes
 app.get('/', (req, res) => {
   res.render('../client/index.html');
 });
